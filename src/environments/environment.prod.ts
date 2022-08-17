6c6c839bcb41703a38c8c45a4fddbd7492527967
Kunal <Kunalsharmarok@gmail.com>
2022-08-17 10:24:45 +0530

    environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  localStorage:"rtl",
-  apiUrl: 'http://13.234.56.70',
+  localStorage: 'rtl',
+  apiUrl: 'http://13.234.56.70:8080',
   fileBaseUrl: 'http://localhost:8080/',
   imageUrl: 'http://localhost:8080/uploads/',
-
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,7 @@
- 
 export const environment = {
   production: false,
-  localStorage:"rtl",
-  apiUrl: 'http://13.234.56.70',
+  localStorage: 'rtl',
+  apiUrl: 'http://13.234.56.70:8080',
   fileBaseUrl: 'http://localhost:8080/',
   imageUrl: 'http://localhost:8080/uploads/',
 };
-
- 
