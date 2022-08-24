b188dc3879d44be179fa16d0bc8a55369ad1e1b6
Kunal <Kunalsharmarok@gmail.com>
2022-08-24 16:35:01 +0530

    employee-transfer

---
+++ src/app/employee/employee-transfer-form/employee-transfer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-transfer-form',
+  templateUrl: './employee-transfer-form.component.html',
+  styleUrls: ['./employee-transfer-form.component.css']
+})
+export class EmployeeTransferFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/employee/employee.routing.ts
+++ src/app/employee/employee.routing.ts
@@ -3,22 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from '../shared/layout/main-layout/main-layout.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeTransferFormComponent } from './employee-transfer-form/employee-transfer-form.component';
 import { EmployeeViewComponent } from './employee-view/employee-view.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: MainLayoutComponent,
-        children: [
-            { path: '', component: EmployeeListComponent },
-            { path: 'form', component: EmployeeFormComponent },
-            { path: 'view:id', component: EmployeeViewComponent }
-        ]
-    },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: EmployeeListComponent },
+      { path: 'form', component: EmployeeFormComponent },
+      { path: 'view:id', component: EmployeeViewComponent },
+      { path: 'transfer', component: EmployeeTransferFormComponent },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class EmployeeRoutingModule { }
+export class EmployeeRoutingModule {}
