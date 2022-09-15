0c993fcb0082eb7bebb08e18daba68eb0f5c851c
Kunal <Kunalsharmarok@gmail.com>
2022-09-15 18:15:09 +0530

    register page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  isLinear = false;
+  firstFormGroup: UntypedFormGroup = Object.create(null);
+  secondFormGroup: UntypedFormGroup = Object.create(null);
+  isOptional = false;
+  isEditable = false;
 
-  constructor() { }
+  // tslint:disable-next-line - Disables all
+  constructor(private _formBuilder: UntypedFormBuilder) { }
 
   ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
   }
 
 }
