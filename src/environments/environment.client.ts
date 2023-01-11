31f31a321e40e6a95337ce52c527263445b08ce8
Kunal <Kunalsharmarok@gmail.com>
2023-01-11 11:36:02 +0530

    ip change

--- src/environments/environment.client.ts
+++ src/environments/environment.client.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   server: '',
   localStorage: 'rtl-livestorage',
-  apiUrl: 'http://roms.rtl.com.au:8080',
-  socketApiUrl: 'http://roms.rtl.com.au:8081',
+  apiUrl: 'http://3.26.1.232:8080',
+  socketApiUrl: 'http://3.26.1.232:8081',
   fileBaseUrl: 'http://localhost:8080/',
   imageUrl: 'http://localhost:8080/uploads/',
   orgId: "ab905406-79a3-4e54-8244-d79fc0e60937"
